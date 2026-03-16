793cb9a3367c154da92c28593113c365df0e07f7
Dyutin Robin <dyutin333@gmail.com>
2026-03-16 01:30:09 +0000

    Implemented loading spinner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   const [preview, setPreview] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
   const [result, setResult] = useState<PredictionResult | null>(null);
-
+  const [statusMessage, setStatusMessage] = useState("Analyzing...");
 
   const formatLabel = (label: string) => {
     return label.replace(/([A-Z])/g, ' $1').trim();
@@ -42,6 +42,12 @@ function App() {
   const uploadImage = async () => {
     if(!file) return
     setLoading(true);
+    setResult(null);
+    setStatusMessage("Waking up the GPU...");
+    const coldStartTimer = setTimeout(() => {
+    setStatusMessage("Server is cold starting (this may take up to 2 mins)...");
+  }, 10000);
+
     try{
        const base64Image = await fileToBase64(file)
 
@@ -58,6 +64,7 @@ function App() {
     } catch(err){
       console.error("Error calling AI:", err)
     } finally {
+      clearTimeout(coldStartTimer);
       setLoading(false);
     }
   }
@@ -96,7 +103,16 @@ function App() {
                 : "bg-blue-600 hover:bg-blue-500 active:scale-95 shadow-lg shadow-blue-900/20"
             }`}
           >
-            {loading ? "Analyzing..." : "Analyze with AI"}          
+            {loading ? (
+              <>
+                <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+                <span>{statusMessage}</span>
+              </>
+              ) : ("Analyze with AI")
+            }       
           </button>
 
           {/* RESULTS SECTION */}
